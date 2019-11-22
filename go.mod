ac984d4b1e710d561c7046cfb3073f598e2a8b0c
crazycs520 <crazycs520@gmail.com>
2019-11-22 16:34:00 +0800

    refine test

--- go.mod
+++ go.mod
@@ -6,4 +6,5 @@ require (
 	github.com/pingcap/kvproto v0.0.0-20191121022655-4c654046831d
 	github.com/shirou/gopsutil v2.19.10+incompatible
 	golang.org/x/sys v0.0.0-20191120155948-bd437916bb0e // indirect
+	google.golang.org/grpc v0.0.0-20180607172857-7a6a684ca69e
 )
--- service_test.go
+++ service_test.go
@@ -1,6 +1,51 @@
 package diagnostics
 
-func setUpService() {
+import (
+	"context"
+	"log"
+	"net"
+	"testing"
+	"time"
+	"google.golang.org/grpc"
 
+	pb "github.com/pingcap/kvproto/pkg/diagnosticspb"
+)
+
+func TestRPCServerLoadInfo(t *testing.T) {
+	address := "127.0.0.1:10080"
+	setUpService(address)
+
+	// Set up a connection to the server.
+	conn, err := grpc.Dial(address, grpc.WithInsecure())
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer conn.Close()
+	c := pb.NewDiagnosticsClient(conn)
+
+	// Contact the server and print out its response.
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+	r, err := c.ServerInfo(ctx, &pb.ServerInfoRequest{Tp:pb.ServerInfoType_LoadInfo})
+	if err != nil {
+		t.Fatal(err)
+	}
+	if r == nil || len(r.Items) == 0 {
+		t.Fatal()
+	}
 	return
+}
+
+func setUpService(addr string){
+	go func() {
+		lis, err := net.Listen("tcp", addr)
+		if err != nil {
+			log.Fatalf("failed to listen: %v", err)
+		}
+		s := grpc.NewServer()
+		pb.RegisterDiagnosticsServer(s, &DiagnoseServer{})
+		if err := s.Serve(lis); err != nil {
+			log.Fatalf("failed to serve: %v", err)
+		}
+	}()
 }
